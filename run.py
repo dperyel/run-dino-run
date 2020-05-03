8ccad170befe0840180dac20096f44c7c443c23d
Denys Periel <denys@periel.biz>
2020-05-03 17:24:22 +0200

    Reconstructing. Adding filest to LFS

--- run.py
+++ run.py
@@ -1,14 +1,41 @@
 """Game runner module"""
-import pyautogui as pag
 
-from dino.search import find_track_region, run_game_loop, dino_search
-from dino.effect import Effects
+# import cv2
+# import numpy as np
+import mss
+import mss.tools
 
 def main():
-    """Starts the game loop"""
-    track_region = find_track_region('assets/dino_day.png', dino_search)
-    effects = Effects(pag)
-    run_game_loop(track_region, effects)
+    """Running the main program"""
 
-if __name__ == '__main__':
+    with mss.mss() as sct:
+
+        monitor1 = {
+            "top": 0,
+            "left": 0,
+            "width": sct.monitors[0]['width'],
+            "height": sct.monitors[0]['height'],
+            "mon": 1
+        }
+
+        monitor2 = {
+            "top": 0,
+            "left": 0,
+            "width": sct.monitors[1]['width'],
+            "height": sct.monitors[1]['height'],
+            "mon": 2
+        }
+
+
+        imgM1 = sct.grab(monitor1)
+        imgM2 = sct.grab(monitor2)
+
+        mss.tools.to_png(imgM1.rgb, imgM1.size, output="test_monitor1.png")
+        mss.tools.to_png(imgM2.rgb, imgM2.size, output="test_monitor2.png")
+
+
+
+
+
+if __name__ == "__main__":
     main()
