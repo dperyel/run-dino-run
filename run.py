13331bb36d7a947e8eb4ec60dbfc18c09a3deca7
Denys Periel <denys@periel.biz>
2020-05-12 09:22:37 +0200

    Adding a linear calculation of the distance when to jump

--- run.py
+++ run.py
@@ -44,6 +44,13 @@ def capture_region(region):
     with mss.mss() as sct:
         monitor_region = {**region, 'monitor': sct.monitors[1]}
         timer = time.time()
+        jump_time = 0.6
+
+        prev_state = {
+            'speed': 0,
+            'time_point': timer,
+            'closest_element': None,
+        }
 
         while True:
             working_area = np.array(sct.grab(monitor_region))
@@ -52,19 +59,32 @@ def capture_region(region):
             edges_on_canvas = find_edges(gray_area)
             features = group_features(edges_on_canvas)
 
+            current_time = time.time()
+
             if len(features) > 0:
-                if features[0][0] < 200:
+                # TODO do not count on the features which are too close
+                if prev_state['closest_element'] and prev_state['closest_element'][0] > prev_state['closest_element'][1]:
+                    d_distance = prev_state['closest_element'][0] - features[0][0]
+                    d_time = current_time - prev_state['time_point']
+                    prev_state['time_point'] = current_time
+                    if d_distance > 0:
+                        speed = d_distance // d_time
+                        prev_state['speed'] = speed if prev_state['speed'] < speed else prev_state['speed']
+
+                prev_state['closest_element'] = features[0]
+
+                # TODO try polynomial to get a better precision 
+                dinamic_distance = 50 + 0.008 * prev_state['speed']
+                if features[0][0] < (dinamic_distance + features[0][1]):
                     pag.press('space')
 
             for left, width in features:
                 cv2.rectangle(gray_area, (left, 1), (left + width, 2 * region['height'] - 1), color=(150, 150, 150), thickness=2)
-
-            text = 'Time: %1.2f' % (time.time() - timer)
+            text = 'Time: %1.2f, Speed: %1.2f' % (time.time() - timer, prev_state['speed'])
             cv2.putText(gray_area, text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
-
             cv2.imshow('frame', gray_area)
 
-            if (cv2.waitKey(1) & 0xFF == ord('q')):
+            if (cv2.waitKey(16) & 0xFF == ord('q')):
                 break
 
         cv2.destroyAllWindows()
